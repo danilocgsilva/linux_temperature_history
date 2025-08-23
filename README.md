b6e70785444c3313daaa541633263e22956cb784
Danilo Silva <danilocgsilva@gmail.com>
2025-08-23 12:31:14 -0300

    README and python check

---
+++ README.md
@@ -0,0 +1,3 @@
+Tracks temperature from a Linux system
+
+Obs.: it requires the `sqlite3` command script to store the data and the python language runtime as well.
--- begin.sh
+++ begin.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 if ! which sqlite3 &> /dev/null; then
-    echo The script requires sqlite3
+    echo The script requires sqlite3.
+    exit
+fi
+
+if ! which python3 &> /dev/null; then
+    echo The script requires python in the system.
     exit
 fi
 
 fetch_data() {
-    sensors | sed 1d; grep -i core
+    sensors | sed 1d | grep -i core
 }
 
 while : ; do fetch_data; sleep 1; done

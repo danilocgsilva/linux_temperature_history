83ca1d19f02c99f91b4829f267dae13e3f918270
Danilo Silva <contact@danilocgsilva.me>
2025-08-24 17:57:22 -0300

    Writting to the buffer correctly

--- begin.sh
+++ begin.sh
@@ -19,23 +19,34 @@ create_tables_if_required() {
     if ! [ -f $STORAGE_FILE_NAME ]; then
         CREATE_TABLE_SCRIPT=$(fetch_data | python3 database_data/generate_table_script.py)
         sqlite3 $STORAGE_FILE_NAME "$CREATE_TABLE_SCRIPT"
-        sqlite3 $STORAGE_FILE_NAME "create table buffer (id INTEGER PRIMARY KEY, inserting_script STRING);"
+        sqlite3 $STORAGE_FILE_NAME "create table buffer (id INTEGER PRIMARY KEY, key STRING, value STRING);"
     fi
 }
 
 insert_date() {
     DATE_STRING=$(date "+%Y-%m-%d %H:%M:%S")
-    INSERTING_SCRIPT="INSERT INTO buffer (entry, value) VALUES ('date', '""$DATE_STRING""');"
-
-    sqlite3 storage.db "INSERT INTO buffer (inserting_script) VALUES (\"""$INSERTING_SCRIPT""\")"
+    sqlite3 storage.db "INSERT INTO buffer (key, value) VALUES ('date', '""$DATE_STRING""')"
 }
 
 insert_data() {
     insert_date
-    fetch_data | python3 database_data/generate_core_insert.py
+    DEFAULT_IFS="$IFS"
+    IFS=$'\n'
+    CORE_INSERTS_RESULTS=($(fetch_data | python3 database_data/generate_core_insert.py))
+    IFS="$DEFAULT_IFS"
+
+    for i in "${CORE_INSERTS_RESULTS[@]}"
+    do
+        sqlite3 storage.db "$i"
+    done
+}
+
+clear_buffer() {
+    sqlite3 storage.db "DELETE FROM buffer;"
 }
 
 create_tables_if_required
+clear_buffer
 insert_data
 # while : ; do
 #     insert_date
--- database_data/generate_core_insert.py
+++ database_data/generate_core_insert.py
@@ -1,13 +1,24 @@
 import sys
 from table_data import get_table_name
+import re
 
 def main():
     for line in sys.stdin:
-        string_to_return = "INSERT INTO buffer () VALUES ();"
-        print(line.strip() + " " + string_to_return)
+        core_name = get_core_name(line)
+        temperature = get_temperature(line)
+        string_to_return = "INSERT INTO buffer (key, value) VALUES ('{}', '{}');".format(core_name, temperature)
+        print(string_to_return)
 
 def get_core_name(line: str) -> str:
     return line.split(':')[0].strip().replace(" ", "_").replace("-", "_").lower()
+
+def get_temperature(sensor_entry: str) -> str:
+    pattern = r':\s*([+-]?\d+\.?\d*)°C'
+    
+    match = re.search(pattern, sensor_entry)
+    if match:
+        return float(match.group(1))
+    return None
 
 if __name__ == "__main__":
     main()
